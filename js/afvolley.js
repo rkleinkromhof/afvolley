26c9438d688eafbcce8b4dd49eb9b2c5e78cd873
Robert Klein Kromhof <rkleinkromhof@gmail.com>
2014-06-29 16:13:08 +0200

    created chat app
    created AngularFire chat app to test AngularJS, Firebase and AngularFire

---
+++ js/afvolley.js
@@ -0,0 +1,12 @@
+var app = angular.module("afvolley", ["firebase"]);
+
+function MyController($scope, $firebase) {
+  var ref = new Firebase("https://afvolley.firebaseio.com/chat");
+  $scope.messages = $firebase(ref);
+  $scope.addMessage = function(e) {
+    if (e.keyCode === 13) {
+      $scope.messages.$add({from: $scope.name, body: $scope.msg});
+      $scope.msg = "";
+    }
+  };
+}
